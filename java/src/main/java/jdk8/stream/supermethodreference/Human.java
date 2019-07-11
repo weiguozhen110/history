378db882a26c88dab25266b3ed97d9ce7d050114
weiguozhen <1962627641@qq.com>
2019-07-12 07:09:29 +0800

    JDK8新特性 lambda 函数式

---
+++ java/src/main/java/jdk8/stream/supermethodreference/Human.java
@@ -0,0 +1,10 @@
+package jdk8.stream.supermethodreference;
+/*
+    定义父类
+ */
+public class Human {
+    //定义一个sayHello的方法
+    public void sayHello(){
+        System.out.println("Hello 我是Human!");
+    }
+}
