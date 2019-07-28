f6d4f08357145de48f10a8935c0fa09d05650ee6
weiguozhen <1962627641@qq.com>
2019-07-28 15:02:31 +0800

    springmvc 模块

---
+++ springmvc/06ssm/06ssm.sql
@@ -0,0 +1,10 @@
+
+create database 06ssm;
+
+use 06ssm;
+
+create table account(
+    id int primary key auto_increment,
+    name varchar(20),
+    money double
+);
---
+++ springmvc/06ssm/src/main/java/springmvc/ssm/dao/AccountDao.java
@@ -0,0 +1,24 @@
+package springmvc.ssm.dao;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+import springmvc.ssm.domain.Account;
+
+import java.util.List;
+
+/**
+ * 帐户dao接口
+ */
+@Repository
+public interface AccountDao {
+
+    // 查询所有账户
+    @Select("select * from account")
+    public List<Account> findAll();
+
+    // 保存帐户信息
+    @Insert("insert into account (name,money) values (#{name},#{money})")
+    public void saveAccount(Account account);
+
+}
